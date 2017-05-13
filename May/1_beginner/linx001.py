57dfcd6003021875842f84bd34b82bcb4c42208c
linx001 <juanbasora89@gmail.com>
2017-05-12 20:48:10 -0400

    Add files via upload

--- May/1_beginner/linx001.py
+++ May/1_beginner/linx001.py
@@ -3,7 +3,7 @@ import os.path
 
 morse_code = {
 	'A':'.-',    'B':'-...',  'C':'-.-.',
-	'D':'-...',  'E':'.', 	  'F':'..-.',
+	'D':'-..',  'E':'.', 	  'F':'..-.',
 	'G':'--.',   'H':'....',  'I':'..',
 	'J':'.---',  'K':'-.-',   'L':'.-..',
 	'M':'--',    'N':'-.',    'O':'---',
@@ -13,7 +13,10 @@ morse_code = {
 	'Y':'-.--',  'Z':'--..',  '0':'-----',
 	'1':'.----', '2':'..---', '3':'...--',
 	'4':'....-', '5':'.....', '6':'-....',
-	'7':'--...', '8':'---..', '9':'----.'
+	'7':'--...', '8':'---..', '9':'----.',
+	'.':'.-.-.-', ',':'--..--', '?':'..--..',
+	'/':'-..-.', '@':'.--.-.', '!':'-.-.--',
+	'&':'.-...', ';':'-.-.-.'
 }
 
 def File_Converter(file):
@@ -26,46 +29,54 @@ def File_Converter(file):
 				else:
 					pass
 	print("\nFile %s has been succesfully converted.\n" % file)
-
 	outf.close()
-	inf.close()
-
 
 def Message_Converter(message):
+	new_message= []
 	for letter in message:
 		if letter.upper() in morse_code:
-			print(morse_code[letter.upper()])
+			new_message.append(morse_code[letter.upper()])
+		else:
+			pass
+	print(' '.join(new_message))
+
+def Message_Decrypter(message):
+	encrypted = message.split()
+	decrypted_message = []
+	for i in encrypted:
+		decrypted_letter = letter_converter(i)
+		decrypted_message.append(decrypted_letter)
+
+	print(''.join(decrypted_message))
+
+#Had to decode the morse code in two different fucntions since
+#doing it in one would print out the letters in alphabetical order.
+def letter_converter(code):
+	for letter, morse in morse_code.items():
+		if code == morse:
+			return letter
 		else:
 			pass
 
 def Main():
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("Message", help='Enter a single world, or a message within "quotes" to convert to morse code')
+	parser.add_argument("Message", help="Enter a single world, or a message within 'quotes' to convert to morse code")
 	parser.add_argument("-F", "--Filename", help='Enter a file name to convert the text to morse code.', action="store_true")
+	parser.add_argument("-D", "--Decrypt", help="Enter morese code within 'quotes' to decrypt to plain text.", action="store_true")
 
 	args = parser.parse_args()
 
-	if args.Filename:
+
+	if args.Decrypt:
+			Message_Decrypter(args.Message)
+	elif args.Filename:
 		if os.path.exists(args.Message):
 			File_Converter(args.Message)
 		else:
-			print("This file does not exitst!")
+			print("\nFile '%s' does not exitst!\n") % args.Message
 	else:
 		Message_Converter(args.Message)
 
-
 if __name__ == '__main__':
 	Main()
-
-
-
-
-
-
-
-
-
-
-
-
